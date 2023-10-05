a6d194d2dc4856f5aa07f866bf60cbc119340bc5
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-10-05 05:06:09 -0400

    Update ComboLine.java

--- src/main/java/YuGiOh/ComboLine.java
+++ src/main/java/YuGiOh/ComboLine.java
@@ -1,42 +1,47 @@
 /**
  * Author: Rayla Kurosaki
  * GitHub: https://github.com/rkp1503
+ * Working Directory:
+ * Command:
  */
-
-
 package YuGiOh;
 
-
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.javatuples.Pair;
 
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 
-
 public class ComboLine {
-	// self.func: any = func
-	private String combo_line = "";
-	private ArrayList<ArrayList<String>> combo_cards_list = new ArrayList<>();
-	private int combo_in_hand_count = 0;
-	private int full_combo_count = 0;
+	private final myFunc combo_function;
+	private final String combo_line;
+	private final ArrayList<ArrayList<String>> combo_cards_list;
+	private int combo_in_hand_count;
+	private int full_combo_count;
+
+	public ComboLine(myFunc combo_function, String combo_line, ArrayList<ArrayList<String>> starting_hand) {
+		this.combo_function = combo_function;
+		this.combo_line = combo_line;
+		this.combo_cards_list = starting_hand;
+		this.combo_in_hand_count = 0;
+		this.full_combo_count = 0;
+	}
+
+	private myFunc get_combo_function() {
+		return this.combo_function;
+	}
 
 	private String get_combo_line() {
 		return this.combo_line;
-	}
-
-	private void set_combo_line(String combo_line) {
-		this.combo_line = combo_line;
 	}
 
 	private ArrayList<ArrayList<String>> get_combo_cards_list() {
 		return this.combo_cards_list;
 	}
 
-	private void add_combo_cards_list(ArrayList<String> combo_cards_list) {
-		this.combo_cards_list.add(combo_cards_list);
+	private void add_combo_cards_list(ArrayList<String> combo_cards) {
+		this.combo_cards_list.add(combo_cards);
 	}
 
 	private int get_combo_in_hand_count() {
@@ -45,6 +50,15 @@ public class ComboLine {
 
 	private void combo_in_hand() {
 		this.combo_in_hand_count += 1;
+	}
+
+	private boolean is_combo_in_hand(ArrayList<String> hand, ArrayList<String> combo_cards) {
+		for (String card : combo_cards) {
+			if (!hand.contains(card)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private int get_full_combo_count() {
@@ -56,50 +70,31 @@ public class ComboLine {
 	}
 
 	private ArrayList<String> copy(ArrayList<String> src) {
-		ArrayList<String> cloned = new ArrayList<>();
-		Collections.copy(cloned, src);
-		return cloned;
+		return new ArrayList<>(src);
 	}
 
-	Pair<Boolean, Boolean> test_hand(ArrayList<String> hand, ArrayList<String> main_deck, ArrayList<String> extra_deck, LinkedHashMap<Integer, JSONObject> db_main, LinkedHashMap<String, Integer> db_helper) {
+	Pair<Boolean, Boolean> test_hand(ArrayList<String> hand, ArrayList<String> main_deck, ArrayList<String> extra_deck, LinkedHashMap<Integer, JSONObject> db_main, LinkedHashMap<String, Integer> db_helper) throws JSONException {
 		boolean cih = false;
 		boolean fc = false;
 		for (ArrayList<String> combo_cards : this.combo_cards_list) {
-			// if all(card in hand for card in combo_cards):
-				// cih: bool = True
-				// fc: bool = self.func(copy.deepcopy(combo_cards), hand, md, ed,
-				// 		db_main, db_helper)
-				// if fc:
-					// if not mp:
-						// self.combo_in_hand_count += 1
-						// self.full_combo_count += 1
-						// pass
-					// else:
-						// with self.combo_in_hand_count_mp.get_lock():
-							// self.combo_in_hand_count_mp.value += 1
-							// pass
-						// with self.full_combo_count_mp.get_lock():
-							// self.full_combo_count_mp.value += 1
-							// pass
-						// pass
-					// break
-					// pass
-				// pass
+			if (is_combo_in_hand(hand, combo_cards)) {
+				ArrayList<String> hand_copy = copy(hand);
+				cih = true;
+				fc = this.combo_function.combo_line(copy(combo_cards), hand, main_deck, extra_deck, db_main, db_helper);
+				if (fc) {
+					this.combo_in_hand_count += 1;
+					this.full_combo_count += 1;
+					break;
+				}
+			}
 		}
-		// if cih and not fc:
-			// if not mp:
-				// self.combo_in_hand_count += 1
-				// pass
-			// else:
-				// with self.combo_in_hand_count_mp.get_lock():
-					// self.combo_in_hand_count_mp.value += 1
-					// pass
-				// pass
-			// pass
+		if (cih && !fc) {
+			this.combo_in_hand_count += 1;
+		}
 		return Pair.with(cih, fc);
 	}
 
-	void print_analysis(int n, int analysis_level, boolean detailed) {
+	void print_analysis(int n) {
 		double p_a = (double) this.full_combo_count / n;
 		double p_b = (double) this.combo_in_hand_count / n;
 		double p_c;
@@ -108,6 +103,6 @@ public class ComboLine {
 		} else {
 			p_c = -1;
 		}
-		System.out.println(this.combo_line + ": [" + String.format("%.2f", p_a) + " | " + String.format("%.2f", p_b) + " | " + String.format("%.2f", p_c) + "]");
+		System.out.println("\t\t" + this.combo_line + ": [" + String.format("%.2f", 100 * p_a) + "% | " + String.format("%.2f", 100 * p_b) + "% | " + String.format("%.2f", 100 * p_c) + "%]");
 	}
 }
