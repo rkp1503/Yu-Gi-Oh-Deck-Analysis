2a1ee239287407c2fe0cff66f62838595630b8da
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-10-05 05:06:15 -0400

    Create myFunc.java

---
+++ src/main/java/YuGiOh/myFunc.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Rayla Kurosaki
+ * GitHub: https://github.com/rkp1503
+ */
+package YuGiOh;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+@FunctionalInterface
+public interface myFunc {
+	boolean combo_line(ArrayList<String> combo_cards, ArrayList<String> hand, ArrayList<String> main_deck, ArrayList<String> extra_deck, LinkedHashMap<Integer, JSONObject> db_main, LinkedHashMap<String, Integer> db_helper) throws JSONException;
+}
